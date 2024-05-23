80d53fa0405fe01d539379b371659e083818e7e3
Abou Ly <166803585+abou94190@users.noreply.github.com>
2024-05-23 10:38:48 +0200

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -2,3 +2,4 @@ double _add(double a, double b);
 double _sub(double a, double b);
 double _mul(double a, double b);
 double _div(double a, double b);
+double _square(double a);  // Ajout de la déclaration de la fonction pour mettre au carré
