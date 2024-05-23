77a21a753f36d2ba624c20f48917fb84b2708550
Abou Ly <166803585+abou94190@users.noreply.github.com>
2024-05-23 10:43:03 +0200

    Update calculator.c

--- src/lib/calculator.c
+++ src/lib/calculator.c
@@ -1,18 +1,28 @@
-#include "./../../include/calculator.h"
+#include "calculator.h"
 
+// Implémentation des fonctions existantes
 double _add(double a, double b) {
-	return a+b;
+    return a + b;
 }
 
 double _sub(double a, double b) {
-	return a-b;
+    return a - b;
 }
 
 double _mul(double a, double b) {
-	return a*b;
+    return a * b;
 }
 
 double _div(double a, double b) {
-	return a/b;
+    if (b == 0) {
+        // Gérer la division par zéro, par exemple en retournant une erreur ou en affichant un message
+        // Ici nous allons simplement retourner 0
+        return 0;
+    }
+    return a / b;
 }
-	
+
+// Implémentation de la nouvelle fonction pour mettre au carré
+double _square(double a) {
+    return a * a;
+}
